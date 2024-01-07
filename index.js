66dd304fdb6ea395da93a46d529e3a7962a5e943
sunilsingh tarkar <sunilthakur89544@gmail.com>
2024-01-07 09:43:54 +0530

    adsense added

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 document.getElementById("resume-button-1").addEventListener("click", function() {
+    
     var pdfUrl = "https://drive.google.com/file/d/12KBH112njcu8D8PsreS4_mR99cVrwhyb/view?usp=sharing";
     window.open(pdfUrl, "_blank");
 });
