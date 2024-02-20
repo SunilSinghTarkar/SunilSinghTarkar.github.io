ea9488a0ed599345f95590e76f7ce68fad298571
sunilsingh tarkar <sunilthakur89544@gmail.com>
2024-02-20 13:41:16 +0530

    navbar changes done

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
-document.getElementById("resume-button-1").addEventListener("click", function() {
-    
-    var pdfUrl = "https://drive.google.com/file/d/12KBH112njcu8D8PsreS4_mR99cVrwhyb/view?usp=sharing";
-    window.open(pdfUrl, "_blank");
+let header = document.querySelector("header");
+let menu = document.querySelector("#menu-icon");
+let navbar = document.querySelector("#nav-menu");
+
+window.addEventListener("scroll", () => {
+  header.classList.toggle("shadow", window.scrollY > 0);
 });
+
+menu.onclick = () => {
+  navbar.classList.toggle("active");
+};
+window.onscroll = () => {
+  navbar.classList.remove("active");
+};
+
+
+      
+
+document.getElementById('resume-link-1').addEventListener("click", () => {
+  console.log("Script file loaded successfully");
+  window.open("https://drive.google.com/file/d/12KBH112njcu8D8PsreS4_mR99cVrwhyb/view?usp=sharing", "_blank");
+});
+
